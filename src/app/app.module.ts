4b57287d9cbcef89b14107681d8696c42c502be1
Meir Donath <donathme@gmail.com>
2019-03-27 11:20:28 -0400

    Added A few folders for Model, Services & Views.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './app/views/search/search.component';
+import { SeasonsComponent } from './views/seasons/seasons.component';
+import { ShowsComponent } from './views/shows/shows.component';
+import { EpisodesComponent } from './views/episodes/episodes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    SeasonsComponent,
+    ShowsComponent,
+    EpisodesComponent
   ],
   imports: [
     BrowserModule,
